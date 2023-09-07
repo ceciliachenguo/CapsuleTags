45a7268ee98e5f46301d7256d4ebcd2e0bc959ef
CeciliaChen <cecilia@pdotf.com>
2023-09-07 19:11:41 -0400

    Initial Commit

---
+++ CapsuleTags/CapsuleTagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapsuleTagsApp.swift
+//  CapsuleTags
+//
+//  Created by Cecilia Chen on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CapsuleTagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
